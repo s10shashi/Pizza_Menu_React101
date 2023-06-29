a3b792110bb0a8846d5c9b318f9881d2e6694171
Shashi Suman <shashiaryavanshi145@gmail.com>
2023-06-29 17:41:59 +0530

    update v 1.02

--- 01_js-review/script.js
+++ 01_js-review/script.js
@@ -1,3 +1,7 @@
+/// install Quokka extension before launching this code in VS Code.
+// launch Quokka==>> open setting //cntl+shfit+ p
+//select quokka start with current file
+
 const data = [
   {
     id: 1,
@@ -66,7 +70,7 @@ const data = [
     publicationDate: "1965-01-01",
     author: "Frank Herbert",
     genres: ["science fiction", "novel", "adventure"],
-    hasMovieAdaptation: true,
+    hasMovieAdaptation: false,
     pages: 658,
     translations: {
       spanish: "",
@@ -145,51 +149,213 @@ function getBook(id) {
 
 // //////////////////////////////////////////////////////////
 
-// // const books = getBooks();
-// // books;
+// // // const books = getBooks();
+// // // books;
 
-// //////////////////////////////////////////////////////////////////
+// // //////////////////////////////////////////////////////////////////
 
-/***************DESTRUCTURING********************** */
+// /***************DESTRUCTURING********************** */
 
-// ////////basic long method///////////
-// //to get book name and author//
+// // ////////basic long method///////////
+// // //to get book name and author//
 
-// // const book= getBook(2);
-// // const title= book.title;
-// // title;// Dune
+// // // const book= getBook(2);
+// // // const title= book.title;
+// // // title;// Dune
 
-// // const author=book.author;
-// // author;
-// // console.log(title,author)
+// // // const author=book.author;
+// // // author;
+// // // console.log(title,author)
 
-// /// METHOD 2 to get title and author at same time
+// // /// METHOD 2 to get title and author at same time
 
-// const book = getBook(2);
-// const { title, author, id, pages, genres } = book;
-// // // console what ever you want
-// // name should be same
+// // const book = getBook(2);
+// // const { title, author, id, pages, genres } = book;
+// // // // console what ever you want
+// // // name should be same
 
-// console.log(author, id, title, genres);
+// // console.log(author, id, title, genres);
 
-// const primary = genres[0];
-// const secondary = genres[1];
-// //console.log(primary,secondary)
+// // const primary = genres[0];
+// // const secondary = genres[1];
+// // //console.log(primary,secondary)
 
-// /// better method->
+// // /// better method->
 
-// const [primaryGenres, secondaryGenres, thirdGenres] = genres;
+// // const [primaryGenres, secondaryGenres, thirdGenres] = genres;
 
-// console.log(primaryGenres, secondaryGenres,thirdGenres);
+// // console.log(primaryGenres, secondaryGenres,thirdGenres);
 
-////////////////////////////////////////////////////////////////////
+// ////////////////////////////////////////////////////////////////////
 
-/*******************REST/SPREAD OPERATOR*****************/
+// /*******************REST/SPREAD OPERATOR*****************/
 
-const book = getBook(2);
-const genres = book.genres;
-const [primaryGenres, secondaryGenres, ...othersGenres] = genres;
+// const book = getBook(3);
+// const {
+//   title,
+//   author,
+//   id,
+//   pages,
+//   genres,
+//   hasMovieAdaptation,
+//   publicationDate,
+// } = book;
+// const [primaryGenres, secondaryGenres, ...othersGenres] = genres;
 
-/*... can be only use to define only the after after that ... */
+// /*... can be only use to define only the after after that ... */
 
-console.log(othersGenres);
+// console.log(othersGenres);
+// /**/ /*******spread operator******** */
+
+// // const newGeners= [genres,"Romantic"];
+// // console.log(newGeners)
+
+// // above method is not used widely because it add seperately, added in the array;
+// // thus we use spread operator;
+
+// const newGeners = [...genres, "Romantic"];
+// console.log(newGeners);
+
+// const updatedBook = {
+//   ...book,
+//   //Adding a new proper only after spread will work..not after
+//   moviePublicationDate: "2011-12-21",
+//   //over writting existing property;
+//   pages: 1221,
+// };
+// console.log(updatedBook);
+
+// //////////////////////////////////////////////////////////////
+// /*************templet literals method*****************/
+
+// const summary = `The name of book is ${book.title} with total pages number as ${
+//   book.pages
+// } is ${hasMovieAdaptation ? "" : "not"} adopetd as a movie`;
+// summary;
+
+// ////////////////////////////////////////////////////////////////
+// /************ternaries (instead of if/else)*****************/
+
+// // 3 part needed
+// // if need condotion(if pages is over 1000)
+// // if true "over 1000"
+// // else "less than 1000"
+
+// console.log(book.pages > 1200 ? "over 1000 pages" : "less then 1000");
+// /// read const summary also to see the usage
+
+// //////////////////////////////////////////////////////
+// /********************Arrow functions************** */
+// // old ways
+
+// // function getYear(str) {
+// //   return str.split("-")[0];
+// // }
+
+// const getYear = (str, a, b) => {
+//   str.split("-")[0];
+// };
+
+// console.log(getYear(publicationDate));
+
+// /////////////////////////////////////////////////
+// /*************short circutting and logical Operator*************/
+
+// /************short Circuting************ */
+
+// console.log(true && "some Stinrg");
+// console.log(false && "some Stirng");
+// console.log(hasMovieAdaptation && "this book has a movie");
+
+// ///falsy value ==>> 0 ,'' ,null ,undefined,
+
+// console.log(true || "some String");
+// console.log(false || "Some value");
+
+// console.log(book.translations.English);
+
+// const englishTranslator = book.translations.English || "Not transrlation";
+// englishTranslator;
+// //// as  we know 0 is a falsy data so if you are short circuiting be sure with tha data that it not have the value zero or else it throws error as false.
+
+// // const count= book.reviews.librarything.reviewsCount ?? "no data";
+// // count;
+
+// //////////////////////////////////////////////////////////
+// /************Optional Chainig****************/
+
+// // taking about getTotalRevieCount(book);
+
+// /** We use below code when we not sure the we get a proper number output // to avoid undefiend and NaN
+//  * with following codeif review count is true it return the number value and short Circuit else return 0 as output
+//  * to avoid undefined or NaN error;
+//  *
+//  */
+
+// function getTotalRevieCount(book) {
+//   const goodreads = book.reviews.goodreads?.reviewsCount ?? 0;
+//   const libCount = book.reviews.librarything?.reviewsCount ?? 0;
+//   return goodreads + libCount;
+// }
+
+// console.log(getTotalRevieCount(book));
+
+// /// go to line#193 and enter 3 as id.. after that you will get as undefined error
+// /*
+// so to over come that we use Optional chainig where we put ? in the last known element (element before the doubtful element) so that if the thing is undefined it will not thrown an error . and return NaN as output
+
+// just put ? after librarythings in function getTotalReviewCount and error will be handled
+// */
+
+/* 2nd time commenting all code*/
+/////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////
+/**
+ * 3 Mthod should be master for React
+ *
+ * map
+ * filter
+ * reduce
+ * do not mutate the original array but return new array based on the original one
+ *
+ */
+
+/************** MAP***********************/
+
+const books = getBooks();
+
+// const x= [1,2,3,4,5,6,7,8,9,10].map((el)=> el*2)
+// console.log(x);
+function getTotalRevieCount(book) {
+  const goodreads = book.reviews.goodreads?.reviewsCount ?? 0;
+  const libCount = book.reviews.librarything?.reviewsCount ?? 0;
+  return goodreads + libCount;
+}
+
+const titles = books.map((book) => book.title);
+//console.log(titles);
+
+const essentialData = books.map((book) => ({
+  titles: book.title,
+  author: book.author,
+  reviewCount: getTotalRevieCount(book),
+}));
+
+/**mif we use ( ) breckets before {} in function then we dont have to use the syntex return as it return everything */
+
+//console.log(essentialData);
+
+//////////****************FILTER METHOD************** */
+
+const longBook = books
+  .filter((book) => book.pages > 500)
+  .filter((book) => book.hasMovieAdaptation);
+console.log(longBook);
+
+const adventureBook = books
+  .filter((book) => book.genres.includes("adventure"))
+  .map((book) => book.title);
+console.log(adventureBook);
