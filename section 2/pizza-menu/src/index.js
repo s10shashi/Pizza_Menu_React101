b74d7a9e5483259b425e7daa27135973f39736c1
Shashi Suman <shashiaryavanshi145@gmail.com>
2023-07-15 17:46:48 +0530

    changes 2.0

--- section 2/pizza-menu/src/index.js
+++ section 2/pizza-menu/src/index.js
@@ -46,6 +46,20 @@ const pizzaData = [
     photoName: "pizzas/prosciutto.jpg",
     soldOut: false,
   },
+  {
+    name: "Pizza Forest",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 20,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza 4 cheese",
+    ingredients: "Aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
 ];
 //
 function App() {
@@ -57,14 +71,70 @@ function App() {
     </div>
   );
 }
+function Pizza({ pizzaObj }) {
+  console.log(pizzaObj);
+
+  // if (pizzaObj.soldOut) return null;
+
+  return (
+    <li className={`pizza ${pizzaObj.soldOut ? "sold-out " : ""}`}>
+      <img src={pizzaObj.photoName} alt={pizzaObj.name} />
+      <div>
+        <h3>{pizzaObj.name}</h3>
+        <p>T{pizzaObj.ingredients}</p>
+        {/* { { pizzaObj.soldOut ? (<span>SOLDOUT</span>): (<span>{pizzaObj.price}</span>)}} */}
+        <span>{pizzaObj.soldOut ? " SOLD OUT " : pizzaObj.price}</span>
+      </div>
+    </li>
+  );
+}
 
 function Menu() {
+  const pizzas = pizzaData;
+  //const pizzas = [];
+  const numPizzas = pizzas.length;
   return (
     <main className="menu">
       <h2>Our Menu</h2>
-      <Pizza />
-      <Pizza />
-      <Pizza />
+      {/* new ternarie method */}
+      {/* <p>
+        Authentic Italian cuisine. 6 creative pizzas to choose from. All from
+        our stone oven, all organic, all Delicious.
+      </p> */}
+
+      {numPizzas > 0 ? (
+        // <React.Fragment >
+        <>
+          <p>
+            Authentic Italian cuisine. 6 creative pizzas to choose from. All
+            from our stone oven, all organic, all Delicious.
+          </p>
+          <ul className="pizzas">
+            {pizzas.map((pizza) => (
+              <Pizza pizzaObj={pizza} key={pizza.name} />
+            ))}
+          </ul>
+          {/* </React.Fragment> */}
+        </>
+      ) : (
+        <p>We're working on it, Please come after somethime !</p>
+      )}
+      {/* old method */}
+      {/* {numPizzas > 0 && (
+        <ul className="pizzas">
+          {pizzas.map((pizza) => (
+            <Pizza pizzaObj={pizza} key={pizza.name} />
+          ))}
+        </ul>
+      )} */}
+
+      {/* <Pizza
+        name="Pizza Spinaci"
+        ingredients="Tomato, mozarella, spinach, and ricotta cheese"
+        photoName="pizzas/spinaci.jpg"
+        price={10}
+      />
+       */}
     </main>
   );
 }
@@ -81,7 +151,7 @@ function Header() {
 
 function Footer() {
   const hour = new Date().getHours();
-  const openHour = 12;
+  const openHour = 10;
   const closeHour = 22;
   const isOpen = hour >= openHour && hour <= closeHour;
   console.log(isOpen);
@@ -89,20 +159,24 @@ function Footer() {
   // else alert("Sorry We're close at this Movement");
   return (
     <footer className="footer">
-      {new Date().toLocaleTimeString()} We're currently open !
+      {isOpen ? (
+        <Order closeHour={closeHour} openHour={openHour} />
+      ) : (
+        <p>
+          We're happy to welcome you between {openHour}:00 to {closeHour}:00{" "}
+        </p>
+      )}
     </footer>
   );
 }
-
-function Pizza() {
+function Order({ closeHour, openHour }) {
   return (
-    <div>
-      <br></br>
-      <img src="pizzas/Spinaci.jpg" alt="Pizza spinaci"></img>
-      <h3>Pizza Spinaci</h3>
-      <p></p>
-      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
-      <br></br>
+    <div className="order">
+      <p>
+        We're Open from {openHour}:00 to {closeHour}:00. Please Coome Visit Us
+        on Order Online
+      </p>
+      <button className="btn"> Order Now</button>
     </div>
   );
 }
